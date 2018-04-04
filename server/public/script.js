ab35bb9e0dfc80c8622ea18d9e35c3a0291857c5
polygeekism <mcmahon.kevin.t@gmail.com>
2018-04-03 22:38:57 -0500

    button linked with app and controller

--- server/public/script.js
+++ server/public/script.js
@@ -1,1 +1,14 @@
-console.log('WE have script');
+var myApp = angular.module('myApp', []);
+
+myApp.controller('submitDayController', function(){
+    console.log('submit service loaded');
+    var self = this;
+
+    
+    
+    self.submitDay = function(){
+        console.log('we have button action')
+    }
+
+})
+
